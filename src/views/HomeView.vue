be19469fa99549bce7acf85c63a8d05a773ecae8
dt <vakarchuk.nik@bk.ru>
2023-01-04 20:44:10 +0300

    ajhdh

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-for="i in react" :key="i.name">
+      <img :src="i.logo"  /> {{i.name}}
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import s from "../../event"
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      react: s
+    }
   }
 }
 </script>
